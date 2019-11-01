c1d120387d645cd7c38d831e0fede931e372cf61
zzell <sukhodolskyi.97@gmail.com>
2019-11-02 01:22:16 +0200

    improve

--- golang.el
+++ golang.el
@@ -1,18 +1,17 @@
 ;;; package --- Summary
 ;;; Commentary:
+;;; Code:
 
 (let ((tools '(("gopls"         . "GO111MODULE=on go get golang.org/x/tools/gopls@latest")
-               ("gocode"        . "go get -u github.com/nsf/gocode")
+               ("gocode"        . "go get -u github.com/mdempsky/gocode")
                ("golangci-lint" . "curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh| sh -s -- -b $(go env GOPATH)/bin v1.21.0")
                ("goimports"     . "go get -u -v golang.org/x/tools/cmd/goimports")
                ("godef"         . "go get github.com/rogpeppe/godef")
                ("guru"          . "go get -u -v golang.org/x/tools/cmd/guru")
                ("gorename"      . "go get -u -v golang.org/x/tools/cmd/gorename")
                ("fillstruct"    . "go get -u github.com/davidrjenni/reftools/cmd/fillstruct")
+               ("gomodifytags"  . "go get -u github.com/fatih/gomodifytags"))))
 
-               ("gomodifytags"  . "go get -u https://github.com/fatih/gomodifytags")
-               ("gore"          . "GO111MODULE=off go get -u github.com/motemen/gore/cmd/gore") ;; TODO how to use?
-               )))
   (mapc (lambda (element)
           (let ((name (car element)) (cmd (cdr element)))
             (when (equal
@@ -20,58 +19,29 @@
                     (format "%s%s%s" "if command -v " name " >/dev/null; then printf 0; else printf 1; fi")) "1")
               (progn (message "installing %s..." name) (shell-command-to-string cmd))))) tools))
 
-
-(setq-default gofmt-command "goimports")
-
 (add-hook 'go-mode-hook (lambda ()
                           ;; not the greates solution, but works
                           (add-to-list 'flycheck-checkers 'golangci-lint)
                           (flycheck-add-next-checker 'lsp-ui '(t . golangci-lint))
-                          (add-hook 'lsp-eldoc-hook 'go-eldoc-setup)
                           (aggressive-indent-mode t)
                           (add-hook 'before-save-hook 'gofmt-before-save)
-                          (lsp)
-                          ))
-;;; Code:
-(use-package go-mode)
+                          (add-hook 'lsp-eldoc-hook 'go-eldoc-setup)
+                          (lsp)))
 
-;; ;; go-code refrence
-;; (use-package go-guru
-;;   :config
-;;   (add-hook 'go-mode-hook 'go-guru-hl-identifier-mode)
-;;   ;; (add-to-list 'display-buffer-alist
-;;   ;;              `(,(rx bos "*go-guru-output*" eos)
-;;   ;;                (display-buffer-reuse-window
-;;   ;;                 display-buffer-in-side-window)
-;;   ;;                (side            . bottom)
-;;   ;;                (reusable-frames . visible)
-;;   ;;                (window-height   . 0.33)))
-;;   )
-
+(use-package go-mode
+  :config (setq-default gofmt-command "goimports"))
 (use-package go-fill-struct)
-
-;; quick docs in minibuffer
 (use-package go-eldoc)
-
-;; linter
-;; golangci-lint can't run linters on fly, because it needs a file-tree to analyze
+(use-package go-tag)
+(use-package go-add-tags) ;; experimental
+(use-package go-playground)
+(use-package go-stacktracer)
 (use-package flycheck-golangci-lint
-  ;; :hook (go-mode . flycheck-golangci-lint-setup)
   :config
   (setq flycheck-golangci-lint-enable-all t)
-  (setq flycheck-golangci-lint-disable-linters '("lll" "dupl" "maligned" "prealloc" "gochecknoglobals" "gosec"))
-  (setq flycheck-golangci-lint-fast t))
-
-;; ;; autocomplete
-;; (use-package company-go
-;;   :config
-;;   (add-to-list 'company-backends 'company-go)
-;;   ;; (add-hook 'go-mode-hook
-;;   ;;           (lambda ()
-;;   ;;             (set (make-local-variable 'company-backends) '(company-go))
-;;   ;;             ;; (company-mode)
-;;   ;;             ))
-;;   )
-
+  (setq flycheck-golangci-lint-fast t)
+  (setq flycheck-golangci-lint-disable-linters '("lll" "dupl" "maligned"
+                                                 "prealloc" "gochecknoglobals" "gosec"
+                                                 "funlen" "wsl" "gocognit")))
 
 ;;; golang.el ends here
--- init.el
+++ init.el
@@ -178,17 +178,25 @@
                             (aggressive-indent-mode t)))
 
 ;; ###########################################
+
 (use-package lsp-mode
   :config
   (setq lsp-prefer-flymake nil))
 
+;; lsp-ui-peek-find-references
+;; lsp-ui-jump-forward
+;; lsp-ui-jump-backward
+;; lsp-ui-flycheck-list
+;; lsp-rename !!!
+
 (use-package lsp-ui
   :config
   (setq lsp-ui-doc-enable nil)
-  (setq lsp-ui-sideline-enable nil))
+  (setq lsp-ui-sideline-enable nil)
+  (setq lsp-ui-flycheck-enable t))
 
 (use-package yasnippet
-  :diminish yasnippet-mode)
+  :diminish yas-minor-mode)
 
 ;; autocomplete
 (use-package company
@@ -528,7 +536,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(yasnippet yaml-mode xclip which-key use-package restclient request rainbow-delimiters protobuf-mode persp-mode paren-face nord-theme neotree minimap lsp-haskell kaolin-themes json-mode js2-mode indent-guide highlight-parentheses highlight-indentation highlight-indent-guides gruvbox-theme go-guru go-fill-struct go-eldoc go-autocomplete ggtags general focus flycheck-status-emoji flycheck-pos-tip flycheck-golangci-lint flx eyebrowse exec-path-from-shell evil-magit evil-escape evil-commentary evil-cleverparens dumb-jump doom-themes doom dockerfile-mode diminish diff-hl darktooth-theme counsel-projectile company-lsp company-go company-ebdb color-theme-sanityinc-tomorrow avy aggressive-indent)))
+   '(go-stacktracer go-rename go-playground go-add-tags go-tag gorepl-mode gore-mode yasnippet yaml-mode xclip which-key use-package restclient request rainbow-delimiters protobuf-mode persp-mode paren-face nord-theme neotree minimap lsp-haskell kaolin-themes json-mode js2-mode indent-guide highlight-parentheses highlight-indentation highlight-indent-guides gruvbox-theme go-guru go-fill-struct go-eldoc go-autocomplete ggtags general focus flycheck-status-emoji flycheck-pos-tip flycheck-golangci-lint flx eyebrowse exec-path-from-shell evil-magit evil-escape evil-commentary evil-cleverparens dumb-jump doom-themes doom dockerfile-mode diminish diff-hl darktooth-theme counsel-projectile company-lsp company-go company-ebdb color-theme-sanityinc-tomorrow avy aggressive-indent)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
