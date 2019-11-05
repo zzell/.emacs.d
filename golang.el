9bf12de8dc59eabec6b241d1b87a29e4c4ab29a0
zzell <sukhodolskyi.97@gmail.com>
2019-11-05 18:54:12 +0200

    add custom go-imports-add fn

--- golang.el
+++ golang.el
@@ -45,4 +45,62 @@
                                                  "prealloc" "gochecknoglobals" "gosec"
                                                  "funlen" "wsl" "gocognit")))
 
+
+(setq go-packages-function 'blb)
+
+(defvar *custom-go-cached-imports* (list))
+
+(defun blb ()
+  "Blank."
+  *custom-go-cached-imports*)
+
+(start-process "qwer" "quer-buffer" 'custom-go-imports)
+
+(defun custom-go-imports ()
+  "Blank."
+  (interactive)
+
+  (let ((root (custom-go-find-project-root)))
+    (if root
+        (progn
+          (setq *custom-go-cached-imports* (list))
+          (setq *custom-go-cached-imports* (append *custom-go-cached-imports* (go-packages-native)))
+          (setq *custom-go-cached-imports* (append *custom-go-cached-imports* (custom-go-local-imports-list root)))
+          (setq *custom-go-cached-imports* (append *custom-go-cached-imports* (custom-go-vendored-imports-list root))))
+      (setq *custom-go-cached-imports* (list))
+      (setq *custom-go-cached-imports* (append *custom-go-cached-imports* (custom-go-all-imports)))))
+
+  )
+
+(defvar *custom-go-gopath* (shell-command-to-string "printf $GOPATH"))
+
+(defun custom-go-find-project-root ()
+  "Return project root or nil if project is not in GOPATH."
+  (let ((gopath-src (concat *custom-go-gopath* "/src"))
+        (pwd (shell-command-to-string "pwd | tr -d '\n'")))
+    (when (string-match-p gopath-src pwd)
+      (let ((elems (split-string (substring pwd (1+ (length gopath-src)) nil) "/")))
+        (when (>= (length elems) 3)
+          (concat (nth 0 elems) "/" (nth 1 elems) "/" (nth 2 elems)))))))
+
+(defun custom-go-local-imports-list (path)
+  "Fast. PATH - project root path."
+  (process-lines "go" "list" "-e" (concat path "/...")))
+
+(defun custom-go-vendored-imports-list (path)
+  "Slow. PATH - project root path."
+  (let* ((absolute-path (concat *custom-go-gopath* "/src/" path))
+         (vendor-path (shell-command-to-string (format "find %S -type d -name 'vendor' | tr -d '\n'" absolute-path))))
+    (unless (string= vendor-path "")
+      (let ((imports (process-lines "go" "list" "-e"
+                                    (concat (substring vendor-path
+                                                       (length (concat *custom-go-gopath* "/src/"))) "/...")))
+            (unprefix (list)))
+        (dolist (i imports) (push (replace-regexp-in-string "^.*\/vendor\/" "" i) unprefix)) unprefix))))
+
+(defun custom-go-all-imports ()
+  "Extremely slow."
+  (process-lines "go" "list" "-e" "all"))
+
+
 ;;; golang.el ends here
