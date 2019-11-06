a8d5686e6de04715a57cbffeb7240726c48951ce
zzell <sukhodolskyi.97@gmail.com>
2019-11-06 19:59:34 +0200

    ugh

--- golang.el
+++ golang.el
@@ -46,61 +46,175 @@
                                                  "funlen" "wsl" "gocognit")))
 
 
-(setq go-packages-function 'blb)
 
-(defvar *custom-go-cached-imports* (list))
 
-(defun blb ()
-  "Blank."
-  *custom-go-cached-imports*)
 
-(start-process "qwer" "quer-buffer" 'custom-go-imports)
 
-(defun custom-go-imports ()
+
+
+
+(let ((proc (start-process-shell-command "proc" "test" "sleep 2 && ls")))
+  (set-process-filter proc '(lambda (x y) (message "%S:%s" x y)))
+  )
+
+;; (defun lsp--info (format &rest args)
+;;   "Display lsp info message with FORMAT with ARGS."
+;;   (message "%s :: %s" (propertize "LSP" 'face 'success) (apply #'format format args)))
+
+;; (lsp--info "%S" "yet")
+
+
+
+;; (setq go-packages-function 'blb)
+;; (defun blb ()
+;;   "Blank."
+;;   *custom-go-cached-imports*)
+
+;; (defvar *custom-go-cached-imports* (list))
+;; (defvar *custom-go-gopath* (shell-command-to-string "printf $GOPATH"))
+
+;; (let ((root (custom-go-find-project-root)))
+;;   (if root
+;;       (progn
+;;         (setq *custom-go-cached-imports* (list))
+;;         (setq *custom-go-cached-imports* (append *custom-go-cached-imports* (go-packages-native)))
+;;         (setq *custom-go-cached-imports* (append *custom-go-cached-imports* (custom-go-local-imports-list root)))
+;;         (setq *custom-go-cached-imports* (append *custom-go-cached-imports* (custom-go-vendored-imports-list root))))
+;;     (setq *custom-go-cached-imports* (list))
+;;     (setq *custom-go-cached-imports* (append *custom-go-cached-imports* (custom-go-all-imports)))))
+
+
+(defvar *gocustom-cached-imports* (list))
+(defvar *gocustom-gopath* (shell-command-to-string "printf $GOPATH"))
+(defvar *gocustom-project-root*)
+
+(defun gocustom--append-to-cache (imports)
+  "IMPORTS."
+  (setq *gocustom-cached-imports* (append *gocustom-cached-imports* imports))
+  )
+
+(defun custom-go-refresh-imports ()
   "Blank."
   (interactive)
 
-  (let ((root (custom-go-find-project-root)))
-    (if root
-        (progn
-          (setq *custom-go-cached-imports* (list))
-          (setq *custom-go-cached-imports* (append *custom-go-cached-imports* (go-packages-native)))
-          (setq *custom-go-cached-imports* (append *custom-go-cached-imports* (custom-go-local-imports-list root)))
-          (setq *custom-go-cached-imports* (append *custom-go-cached-imports* (custom-go-vendored-imports-list root))))
-      (setq *custom-go-cached-imports* (list))
-      (setq *custom-go-cached-imports* (append *custom-go-cached-imports* (custom-go-all-imports)))))
+  (let ((root (gocustom--find-package-root)))
+    (if root (progn
+               (setq *gocustom-cached-imports* (list))
+               (gocustom--load-local-imports
+                "github.com/ContinuumLLC/platform-brightgauge-plugin"
+                '(lambda (imports)
+                   (gocustom--append-to-cache imports)
+                   (message "%S" "loaded local imports")))
+               )
 
-  )
+      )
+    ))
 
-(defvar *custom-go-gopath* (shell-command-to-string "printf $GOPATH"))
+;; (gocustom--load-vendored-imports "github.com/ContinuumLLC/platform-brightgauge-plugin" '(lambda (im) (message "%S" im)))
 
-(defun custom-go-find-project-root ()
-  "Return project root or nil if project is not in GOPATH."
-  (let ((gopath-src (concat *custom-go-gopath* "/src"))
+(defun gocustom--find-package-root ()
+  "Return projects root in form 'github.com/user/project' or nil if project is not in GOPATH."
+  (let ((gopath-src (concat *gocustom-gopath* "/src"))
         (pwd (shell-command-to-string "pwd | tr -d '\n'")))
     (when (string-match-p gopath-src pwd)
       (let ((elems (split-string (substring pwd (1+ (length gopath-src)) nil) "/")))
         (when (>= (length elems) 3)
           (concat (nth 0 elems) "/" (nth 1 elems) "/" (nth 2 elems)))))))
 
-(defun custom-go-local-imports-list (path)
-  "Fast. PATH - project root path."
-  (process-lines "go" "list" "-e" (concat path "/...")))
+(defun gocustom--load-local-imports (root filter)
+  "Asynchronously load local imports per package. ROOT - package root path.
+FILTER - callback function that receives list of packages."
+  (process-shell-command-stdout (format "go list -e %S/..." root) filter))
 
-(defun custom-go-vendored-imports-list (path)
-  "Slow. PATH - project root path."
-  (let* ((absolute-path (concat *custom-go-gopath* "/src/" path))
+(defun gocustom--load-vendored-imports (root filter)
+  ;; "May be slow. Load vendored imports (e.g. when using Glide). ROOT - project root."
+
+  (let* ((absolute-path (concat *gocustom-gopath* "/src/" root))
          (vendor-path (shell-command-to-string (format "find %S -type d -name 'vendor' | tr -d '\n'" absolute-path))))
     (unless (string= vendor-path "")
-      (let ((imports (process-lines "go" "list" "-e"
-                                    (concat (substring vendor-path
-                                                       (length (concat *custom-go-gopath* "/src/"))) "/...")))
-            (unprefix (list)))
-        (dolist (i imports) (push (replace-regexp-in-string "^.*\/vendor\/" "" i) unprefix)) unprefix))))
+      (process-shell-command-stdout
+       (format "go list -e %S/..." (replace-regexp-in-string "^.*?\/src\/" "" vendor-path))
+       `(lambda (imports)
+          (let ((unprefix (list)))
+            (dolist (i imports) (push (replace-regexp-in-string "^.*?\/vendor\/" "" i) unprefix))
+            (funcall ,filter unprefix)))))))
 
-(defun custom-go-all-imports ()
+(async-shell-command-to-string "go list -e github.com/ContinuumLLC/platform-brightgauge-plugin/src/vendor/..." '(lambda (out) (message "%S" out)))
+
+;; (message "%S" (process-lines "go" "list" "-e" "github.com/ContinuumLLC/platform-brightgauge-plugin/src/vendor/..."))
+
+(defun async-shell-command-to-string (command callback)
+  "Execute shell command COMMAND asynchronously in the background.
+
+Return the temporary output buffer which command is writing to
+during execution.
+
+When the command is finished, call CALLBACK with the resulting
+output as a string."
+  (let*
+      ((output-buffer (generate-new-buffer " *temp*"))
+       (callback-fun callback))
+    (set-process-sentinel
+     (start-process "Shell" output-buffer shell-file-name shell-command-switch command)
+     `(lambda (process signal)
+        (when (memq (process-status process) '(exit signal))
+          (with-current-buffer output-buffer
+            (let ((output-string
+                   (buffer-substring-no-properties
+                    (point-min)
+                    (point-max))))
+              (funcall callback-fun output-string)))
+          (kill-buffer output-buffer))))
+    output-buffer))
+
+;; (defun process-shell-command-stdout (cmd filter)
+;;   "Run CMD asynchronously and call FILTER on exit.
+;; FILTER is callback function that receives one parameter
+;; - list of lines from CMD stdout."
+
+;;   (let ((proc (start-process-shell-command (number-to-string (random)) nil cmd)))
+
+
+
+;;     (set-process-filter
+;;      proc
+;;      `(lambda (_ stdout)
+;;         (with-temp-buffer
+;;           (insert stdout)
+;;           (goto-char (point-min))
+;;           (let (lines)
+;; 	          (while (not (eobp))
+;; 	            (setq lines (cons (buffer-substring-no-properties
+;; 			                           (line-beginning-position)
+;; 			                           (line-end-position)) lines))
+;; 	            (forward-line 1))
+;;             (funcall ,filter (nreverse lines))))))
+
+;;     )
+
+;;   )
+
+
+;; (defun process-shell-command-stdout (cmd filter)
+;;   "Run CMD asynchronously and call FILTER on exit.
+;; FILTER is callback function that receives one parameter
+;; - list of lines from CMD stdout."
+;;   (set-process-filter
+;;    (start-process-shell-command (number-to-string (random)) nil cmd)
+;;    `(lambda (_ stdout)
+;;       (with-temp-buffer
+;;         (insert stdout)
+;;         (goto-char (point-min))
+;;         (let (lines)
+;; 	        (while (not (eobp))
+;; 	          (setq lines (cons (buffer-substring-no-properties
+;; 			                         (line-beginning-position)
+;; 			                         (line-end-position)) lines))
+;; 	          (forward-line 1))
+;;           (funcall ,filter (nreverse lines)))))))
+
+(defun gocustom--load-all-imports ()
   "Extremely slow."
   (process-lines "go" "list" "-e" "all"))
-
 
 ;;; golang.el ends here
